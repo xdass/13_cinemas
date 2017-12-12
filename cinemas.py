48d7706019543b10b32d83b40e4a237a1d1844cb
Dass <420600@gmail.com>
2017-12-12 23:10:43 +0500

    Parse list of films. Add html content from afisha for developing.

--- cinemas.py
+++ cinemas.py
@@ -1,3 +1,10 @@
+import bs4
+import requests
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+
+# .m-disp-table h3 a Film tag
 
 
 def fetch_afisha_page():
@@ -17,4 +24,16 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    items = []
+    with open('content_for_test.html', encoding='utf-8') as file:
+        soup = bs4.BeautifulSoup(file, 'html.parser')
+    d = soup.select('.b-theme-schedule.b-theme-schedule .object')
+    for item in d:
+        film_name = item.select('.m-disp-table h3 a[href]')[0].string
+        lst = item.select('.m-disp-table + table tr .b-td-item a')
+        cinemas = [item.string for item in lst]
+        items.append({
+            'film_name': film_name,
+            'cinemas': cinemas,
+            'cinemas_count': len(cinemas)
+        })
