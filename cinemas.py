cf4088e4b4c5a21c56424868bd5442065f1dd1d5
Dass <420600@gmail.com>
2017-12-24 19:29:32 +0500

    Refactor functions PEP8 fixes

--- cinemas.py
+++ cinemas.py
@@ -1,40 +1,41 @@
-import time
 from operator import itemgetter
 import argparse
 import bs4
 import requests
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
-
 
 def fetch_afisha_page():
-    response = requests.get('https://www.afisha.ru/msk/schedule_cinema/', headers={'user-agent': USER_AGENT})
+    url = 'https://www.afisha.ru/msk/schedule_cinema/'
+    response = make_data_request(url)
     return response.text
 
 
-def parse_afisha_list(raw_html):
+def make_data_request(url, params=None):
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) ' \
+                 'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                 'Chrome/62.0.3202.94 Safari/537.36'
+    response = requests.get(url, params=params, headers={'User-Agent': user_agent})
+    return response
+
+
+def parse_afisha_html(raw_html, movies_to_parse):
     movies_info = []
     soup = bs4.BeautifulSoup(raw_html, 'html.parser')
     movies_html = soup.select('.b-theme-schedule.b-theme-schedule .object')
-    for movie_html in movies_html:
-        film_name = movie_html.select('.m-disp-table h3 a[href]')[0].string
+    for movie_html in movies_html[:movies_to_parse]:
+        film_name = movie_html.select_one('.m-disp-table h3 a[href]').string
         cinemas_ahref_list = movie_html.select('.m-disp-table + table tr .b-td-item a')
-        cinemas = [cinema_ahref.string for cinema_ahref in cinemas_ahref_list]
         movies_info.append({
             'film_name': film_name,
-            'cinemas': cinemas,
-            'cinemas_count': len(cinemas)
+            'cinemas_count': len(cinemas_ahref_list)
         })
     return movies_info
 
 
-def get_movie_id(movie_title):
+def fetch_movie_id(movie_title):
+    url = 'https://www.kinopoisk.ru/search/suggest'
     params = {'q': movie_title, 'topsuggest': 'true', 'ajax': 1}
-    response = requests.get(
-        'https://www.kinopoisk.ru/search/suggest',
-        headers={'user-agent': USER_AGENT},
-        params=params
-    )
+    response = make_data_request(url, params)
     movie_json_info = response.json()
     if movie_json_info:
         if movie_json_info[0]['dataType'] == 'film':
@@ -45,31 +46,32 @@ def get_movie_id(movie_title):
         return None
 
 
-def get_movie_rating(movie_id):
-    response = requests.get('https://rating.kinopoisk.ru/{}.xml'.format(movie_id))
+def fetch_movie_rating(movie_id):
+    url = 'https://rating.kinopoisk.ru/{}.xml'.format(movie_id)
+    response = make_data_request(url)
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     rating = soup.kp_rating.text
     num_voice = soup.kp_rating['num_vote']
     return num_voice, rating
 
 
-def collect_full_movie_info(movies):
+def collect_full_movies_info(movies):
     full_movies_info = []
-    for film in movies:
-        film_id = get_movie_id(film['film_name'])
+    for movie in movies:
+        film_id = fetch_movie_id(movie['film_name'])
         if film_id:
-            num_voice, rating = get_movie_rating(film_id)
-            film['num_voice'] = num_voice
-            film['rating'] = rating
-            full_movies_info.append(film)
+            num_voice, rating = fetch_movie_rating(film_id)
+            movie['num_voice'] = num_voice
+            movie['rating'] = rating
+            full_movies_info.append(movie)
     return full_movies_info
 
 
-def output_movies_to_console(movies, movies_to_print, min_in_cinemas):
+def output_movies_to_console(movies, min_in_cinemas):
     movies.sort(key=itemgetter('rating'), reverse=True)
     if min_in_cinemas:
         movies = [movie_info for movie_info in movies if movie_info['cinemas_count'] > min_in_cinemas]
-    for movie in movies[:movies_to_print]:
+    for movie in movies:
         print(movie['film_name'])
         print('Рейтинг фильма(кинопоиск): {}'.format(movie['rating']))
         print('Проголосовало: {}'.format(movie['num_voice']))
@@ -83,7 +85,7 @@ if __name__ == '__main__':
         '--movies_count',
         default=10,
         type=int,
-        help='Amount of movies that will be print(default is 10)'
+        help='Amount of movies that will be parse(default is 10)'
     )
     parser.add_argument(
         '--min_in',
@@ -95,7 +97,7 @@ if __name__ == '__main__':
     print('Load afisha...')
     raw_html = fetch_afisha_page()
     print('Parsing movies from afisha...')
-    movies_from_afisha = parse_afisha_list(raw_html)
+    movies_from_afisha = parse_afisha_html(raw_html, args.movies_count)
     print('Getting full movies info...')
-    movies_full_data = collect_full_movie_info(movies_from_afisha)
-    output_movies_to_console(movies_full_data, args.movies_count, args.min_in)
+    movies_full_data = collect_full_movies_info(movies_from_afisha)
+    output_movies_to_console(movies_full_data, args.min_in)
